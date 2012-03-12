7c6073ab4d4886b97b2294329259d9c1108158a9
Eric Bardes <ericbardes@gmail.com>
2012-03-11 22:41:25 -0400

    Transition Experiment

--- src/org/bardes/html/BodyHelper.java
+++ src/org/bardes/html/BodyHelper.java
@@ -4,12 +4,27 @@ import javax.servlet.http.HttpServletRequest;
 
 public class BodyHelper
 {
+	private String projector;
+	private String slide;
+
 	public BodyHelper(HttpServletRequest req)
 	{
+		projector = req.getParameter("projector");
+		slide = req.getParameter("slide");
 	}
 	
 	public CharSequence body()
 	{
 		return new java.util.Date().toString();
+	}
+	
+	public CharSequence getContentLink()
+	{
+		return "<img src=\"pages/Untitled."+slide+".png\" />";
+	}
+	
+	public CharSequence getTransitionHeader()
+	{
+		return "body { -webkit-transition: opacity 3s linear; }";
 	}
 }
