2448d43dbf565e4d62ce04196e0b3204dbb719fa
Eric Bardes <ericbardes@gmail.com>
2012-03-16 23:09:36 -0400

    Temporary commit of config changes

---
+++ config.js
@@ -0,0 +1,4 @@
+q(1.5, {});
+q(5, {a: img('title.png')});
+
+q(1, {a: blank, b: blank, c:blank });
---
+++ src/org/bardes/config/DefCue.java
@@ -0,0 +1,50 @@
+package org.bardes.config;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.bardes.entities.Cue;
+import org.bardes.entities.Slide;
+import org.mozilla.javascript.NativeObject;
+
+
+public class DefCue
+{
+	public Set<Cue> cues = new TreeSet<Cue>(); 
+	
+	public Set<Cue> define(String cueName, NativeObject obj)
+	{
+		Object[] allIds = obj.getAllIds();
+		
+		Cue cue = new Cue();
+		cue.setCue(new BigDecimal(cueName));
+		cues.add(cue);
+		
+		System.out.println("Define cue " + cue);
+		
+		for (Object id : allIds)
+		{
+			Slide slide = new Slide();
+			slide.setProjector(String.valueOf(id)); 
+					
+			Object value = obj.get(id);
+			if (value instanceof SlideType)
+			{
+				SlideType type = (SlideType) value;
+				slide.setContentType(type.getType());
+			}
+			System.out.println("  " + id + ": " + slide);
+			
+			cue.addSlide(id.toString(), slide);
+		}
+		
+		return cues;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return cues.toString();
+	}
+}
